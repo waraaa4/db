f6b16c5c956c4c89f4d8acf7a184a69410c3abb2
waraaa4 <warcjh00@naver.com>
2021-12-03 15:29:39 +0900

    123

--- spring_study.sql
+++ spring_study.sql
@@ -54,3 +54,33 @@ create table board_table(
 );
 select * from board_table;
 
+insert into board_table(b_writer,b_password,b_title,b_contents,b_date)
+	values('작성자','123','제목','내용',now());
+    
+-- 1.최근글부터 보여야함.
+select * from board_table order by b_number desc;    
+-- 2.b_number 기준으로 내림차순 정렬한 상태에서 3개씩 글을 조회
+-- limit a, b: a(순서, 가장첫번째가 0), b(한번에 조회할 갯수)
+-- 현재 게시판 테이블에서 3개씩 본다면 1페이지를 보는 쿼리 (0,1,2)
+select * from board_table order by b_number desc limit 0, 3;    
+-- 2페이지를 보는 쿼리 (3,4,5)
+select * from board_table order by b_number desc limit 3, 3;        
+-- 3페이지를 보는 쿼리 (6,7,8)
+select * from board_table order by b_number desc limit 6, 3;        
+-- 4페이지를 보는 쿼리 (9,10,11)
+select * from board_table order by b_number desc limit 9, 3;    
+
+-- 한페이지에 5개씩 글을 조회할수 있다면 1,2,3,4페이지를 어떻게 조회할지 쿼리문을 작성해봅시다. 
+select * from board_table order by b_number desc limit 0, 5;    
+select * from board_table order by b_number desc limit 5, 5;    
+select * from board_table order by b_number desc limit 10, 5;    
+select * from board_table order by b_number desc limit 15, 5;    
+
+-- 제목에 안녕 이라는 단어가 포함된 결과를 출력하는 쿼리는?
+select * from board_table where b_title like '%안녕%';
+-- 내용에 안녕 이라는 단어가 포함된 결과를 출력하는 쿼리는?
+select * from board_table where b_contents like '%안녕%';
+
+
+
+
